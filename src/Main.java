5c824195322d1b285cfc2bd3ec0409d5b19d946d
aum9518 <abduganiuulumanasbek@gmail.com>
2023-02-17 16:11:28 +0600

    add solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] nums={1,8,6,83,12};
+        int target = 84;
+        for (int i=0;i< nums.length-1;i++){
+            for (int j=i+1;j<nums.length;j++){
+                if (nums[i]+nums[j]==target){
+                    System.out.println(i);
+                    System.out.println(j);
+                }
+            }
+        }
     }
 }
